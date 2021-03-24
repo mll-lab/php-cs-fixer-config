55107a2bf4fce2a0353e7e765f522a59edfc2fe1
Benedikt Franke <benedikt.franke@mll.com>
2021-03-24 11:49:41 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,30 @@
+# php-cs-fixer-config
+
+Shared configuration for php-cs-fixer
+
+[![GitHub license](https://img.shields.io/github/license/mll-lab/php-cs-fixer-config.svg)](https://github.com/mll-lab/php-cs-fixer-config/blob/master/LICENSE)
+[![Packagist](https://img.shields.io/packagist/v/mll-lab/php-cs-fixer-config.svg)](https://packagist.org/packages/mll-lab/php-cs-fixer-config)
+[![Packagist](https://img.shields.io/packagist/dt/mll-lab/php-cs-fixer-config.svg)](https://packagist.org/packages/mll-lab/php-cs-fixer-config)
+
+## Installation
+
+    composer require --dev mll-lab/php-cs-fixer-config
+
+## Usage
+
+In your `.php_cs`:
+
+```php
+<?php declare(strict_types=1);
+
+use function MLL\PhpCsFixerRules\config;
+
+$finder = PhpCsFixer\Finder::create()
+    ->notPath('vendor')
+    ->in(__DIR__)
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return config($finder);
+```
---
+++ config.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MLL\PhpCsFixerRules;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+const RULES = [
+    '@Symfony' => true,
+
+    'array_syntax' => [
+        'syntax' => 'short',
+    ],
+    'binary_operator_spaces' => [
+        'default' => 'single_space',
+    ],
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'class_attributes_separation' => [
+        'elements' => [
+            'method',
+            'property',
+        ],
+    ],
+    'heredoc_indentation' => true,
+    'method_argument_space' => [
+        'on_multiline' => 'ensure_fully_multiline',
+    ],
+    'linebreak_after_opening_tag' => true,
+    'new_with_braces' => true,
+    'no_superfluous_phpdoc_tags' => true,
+    'not_operator_with_successor_space' => true,
+    'ordered_imports' => true,
+    'operator_linebreak' => [
+        'position' => 'beginning',
+    ],
+    'phpdoc_order' => true,
+    'phpdoc_align' => [
+        'align' => 'left',
+        'tags' => [
+            'param',
+            'property',
+            'property-read',
+            'property-write',
+            'return',
+            'throws',
+            'type',
+            'var',
+            'method',
+        ],
+    ],
+    'phpdoc_no_alias_tag' => [
+        'type' => 'var',
+        'link' => 'see',
+    ],
+    'phpdoc_no_empty_return' => false,
+    'single_line_throw' => false,
+
+    // Risky rules
+    'declare_strict_types' => true,
+];
+
+/**
+ * Create a php-cs-fixer config that is enhanced with MLL rules.
+ *
+ * @param array<string, mixed> $ruleOverrides Custom rules that override the ones from this config
+ */
+function config(Finder $finder, array $ruleOverrides = []): Config
+{
+    return (new Config())
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules(array_merge(RULES, $ruleOverrides));
+}
