d73ee63e5133957a52eb0e4a35d11fa65f25699e
Simon Bigelmayr <simon.bigelmayr@mll.com>
2021-04-06 12:02:31 +0200

    add fixer, test and php unit

---
+++ src/Fixer/Casing/VariableCaseFixer.php
@@ -0,0 +1,131 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace MLL\PhpCsFixerRules\Fixer\Casing;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\ConfigurationDefinitionFixerInterface;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
+use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Preg;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+/**
+ * Fixer for variables case.
+ *
+ * @author Jennifer Konikowski <jennifer@testdouble.com>
+ */
+final class VariableCaseFixer extends AbstractFixer implements ConfigurationDefinitionFixerInterface
+{
+    /**
+     * @internal
+     */
+    const CAMEL_CASE = 'camel_case';
+
+    /**
+     * @internal
+     */
+    const SNAKE_CASE = 'snake_case';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition()
+    {
+        return new FixerDefinition(
+            'Enforce camel (or snake) case for variable names, following configuration.',
+            [
+                new CodeSample("<?php \$my_variable = 2;\n"),
+                new CodeSample("<?php \$myVariable = 2;\n", ['case' => self::SNAKE_CASE]),
+            ],
+            null,
+            'Risky because it cannot detect a change that will have an impact in other files.'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isRisky()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCandidate(Tokens $tokens)
+    {
+        return $tokens->isAnyTokenKindsFound([T_VARIABLE, T_STRING_VARNAME]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createConfigurationDefinition()
+    {
+        return new FixerConfigurationResolver([
+            (new FixerOptionBuilder('case', 'Apply `camel_case` or `snake_case` to variables.'))
+                ->setAllowedValues([self::CAMEL_CASE, self::SNAKE_CASE])
+                ->setDefault(self::CAMEL_CASE)
+                ->getOption(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens)
+    {
+        foreach ($tokens as $index => $token) {
+            if ((T_VARIABLE === $token->getId()) || (T_STRING_VARNAME === $token->getId())) {
+                $tokens[$index] = new Token([$token->getId(), $this->updateVariableCasing($token->getContent())]);
+            }
+        }
+    }
+
+    private function updateVariableCasing($variableName)
+    {
+        if (self::CAMEL_CASE === $this->configuration['case']) {
+            return $this->camelCase($variableName);
+        }
+
+        return $this->snakeCase($variableName);
+    }
+
+    private function camelCase($string)
+    {
+        $string = Preg::replace('/_/i', ' ', $string);
+        $string = trim($string);
+        // uppercase the first character of each word
+        $string = ucwords($string);
+        $string = str_replace(' ', '', $string);
+
+        return lcfirst($string);
+    }
+
+    private function snakeCase($string, $separator = '_')
+    {
+        // insert separator between any letter and the beginning of a numeric chain
+        $string = Preg::replace('/([a-z]+)([0-9]+)/i', '$1' . $separator . '$2', $string);
+        // insert separator between any lower-to-upper-case letter chain
+        $string = Preg::replace('/([a-z]+)([A-Z]+)/', '$1' . $separator . '$2', $string);
+        // insert separator between the end of a numeric chain and the beginning of an alpha chain
+        $string = Preg::replace('/([0-9]+)([a-z]+)/i', '$1' . $separator . '$2', $string);
+
+        // Lowercase
+        return strtolower($string);
+    }
+}
---
+++ tests/Fixer/Casing/VariableCaseFixerTest.php
@@ -0,0 +1,126 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Tests\Fixer\Casing;
+
+use MLL\PhpCsFixerRules\Fixer\Casing\VariableCaseFixer;
+use PhpCsFixer\Test\AbstractFixerTestCase;
+
+
+/**
+ * @author Jennifer Konikowski <jennifer@testdouble.com>
+ *
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\Casing\VariableCaseFixer
+ */
+final class VariableCaseFixerTest extends  AbstractFixerTestCase
+{
+    /**
+     * @param string $expected
+     * @param string|null $input
+     *
+     * @dataProvider provideCamelCaseFixCases
+     */
+    public function testCamelCaseFix($expected, $input = null)
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideCamelCaseFixCases()
+    {
+        return [
+            [
+                '<?php $testVariable = 2;',
+                '<?php $test_variable = 2;',
+            ],
+            [
+                '<?php $testVariable = 2; echo "hi $testVariable!";',
+                '<?php $test_variable = 2; echo "hi $test_variable!";',
+            ],
+            [
+                '<?php $testVariable = 2; echo "hi ${testVariable}!";',
+                '<?php $test_variable = 2; echo "hi ${test_variable}!";',
+            ],
+            [
+                '<?php $testVariable = 2; echo "hi {$testVariable}!";',
+                '<?php $test_variable = 2; echo "hi {$test_variable}!";',
+            ],
+            [
+                '<?php function foo_bar() { $testVariable = 2;}',
+                '<?php function foo_bar() { $test__variable = 2;}',
+            ],
+            [
+                '<?php echo $testModel->this_field;',
+                '<?php echo $test_model->this_field;',
+            ],
+            [
+                '<?php function f($barBaz, $file) { require $file;}',
+                '<?php function f($bar_baz, $file) { require $file;}',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $expected
+     * @param string|null $input
+     *
+     * @dataProvider provideSnakeCaseFixCases
+     */
+    public function testSnakeCaseFix($expected, $input = null)
+    {
+        $this->fixer->configure(['case' => VariableCaseFixer::SNAKE_CASE]);
+        $this->doTest($expected, $input);
+    }
+
+    public function provideSnakeCaseFixCases()
+    {
+        return [
+            [
+                '<?php $test_variable = 2;',
+                '<?php $testVariable = 2;',
+            ],
+            [
+                '<?php $abc_12_variable = 2;',
+                '<?php $abc12Variable = 2;',
+            ],
+            [
+                '<?php $abc_123_a_variable = 2;',
+                '<?php $abc123aVariable = 2;',
+            ],
+            [
+                '<?php function fooBar() { $test_variable = 2;}',
+                '<?php function fooBar() { $testVariable = 2;}',
+            ],
+            [
+                '<?php $test_variable = 2; echo "hi $test_variable!";',
+                '<?php $testVariable = 2; echo "hi $testVariable!";',
+            ],
+            [
+                '<?php $test_variable = 2; echo "hi ${test_variable}!";',
+                '<?php $testVariable = 2; echo "hi ${testVariable}!";',
+            ],
+            [
+                '<?php $test_variable = 2; echo "hi {$test_variable}!";',
+                '<?php $testVariable = 2; echo "hi {$testVariable}!";',
+            ],
+            [
+                '<?php echo $test_model->this_field;',
+                '<?php echo $testModel->this_field;',
+            ],
+            [
+                '<?php function f($bar_baz, $file) { require $file;}',
+                '<?php function f($barBaz, $file) { require $file;}',
+            ],
+        ];
+    }
+}
